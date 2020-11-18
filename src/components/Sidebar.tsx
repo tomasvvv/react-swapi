c0d74355ea0cca9c4298f4d928d3adc211b83414
Tomas Vosylius <vsls.tomas@gmail.com>
2020-11-19 01:06:21 +0200

    Sidebar stylings

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,15 +1,15 @@
 import {
+  Drawer,
   List,
+  Theme,
   ListItem,
   ListItemText,
-  Theme,
+  Divider,
   makeStyles,
   createStyles,
-  Container,
 } from '@material-ui/core';
 import React from 'react';
 import { ITab } from '../interfaces/ITab';
-import './sidebar.css';
 
 interface Props {
   tabs?: ITab[];
@@ -17,22 +17,70 @@ interface Props {
   setActiveTab?: Function;
 }
 
-const useStyles = makeStyles({
-  root: {
-    width: '100%',
-  },
-});
+const drawerWidth = 200;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    drawerPaper: {
+      top: 'unset',
+      backgroundColor: theme.palette.primary.main,
+      paddingTop: '76px',
+    },
+
+    list: {
+      width: `${drawerWidth}px`,
+      height: '100%',
+      padding: '0',
+    },
+
+    listItem: {
+      height: '42px',
+      color: theme.palette.secondary.main,
+    },
+    selectedListItem: {
+      color: theme.palette.primary.main,
+    },
+
+    listItemText: {
+      textAlign: 'center',
+    },
+    divider: {
+      backgroundColor: theme.palette.secondary.main,
+    },
+  }),
+);
 
 export default function Sidebar({ tabs, activeTab, setActiveTab }: Props): JSX.Element {
   const classes = useStyles();
 
   return (
-    <List classes={{ root: classes.root }} component="nav" aria-label="sidebar">
-      {tabs?.map((tab, index) => (
-        <ListItem>
-          <ListItemText primary={tab.title} />
-        </ListItem>
-      ))}
-    </List>
+    <Drawer
+      variant="persistent"
+      open={true}
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+    >
+      <Divider className={classes.divider} />
+      <List component="nav" className={classes.list}>
+        {tabs?.map((tab, index) => (
+          <>
+            <ListItem
+              classes={{
+                root: classes.listItem,
+                selected: classes.selectedListItem,
+              }}
+              key={index}
+              selected={activeTab === index}
+              button
+              onClick={() => setActiveTab && setActiveTab(index)}
+            >
+              <ListItemText className={classes.listItemText} primary={tab.title} />
+            </ListItem>
+          </>
+        ))}
+        <Divider className={classes.divider} />
+      </List>
+    </Drawer>
   );
 }
